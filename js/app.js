4c4796c6edc0c8f3df886a20a917999e19f3988b
Ricky Nugroho <rocanowi@yahoo.co.id>
2017-12-24 07:35:27 +0700

    Fix set interval not live updating.

--- js/app.js
+++ js/app.js
@@ -1,40 +1,45 @@
-$(function () {
-    const numberWithCommas = (x) => {
-        //https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
-        var parts = x.toString().split(".");
-        parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        return parts.join(".");
-    }
+const numberWithCommas = (x) => {
+    //https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
+
+var update_idr_func;
+
+function update_idr_value(){
+    console.log('Update IDR Value');
+    $('#total-asset').val($('#idr-balance').val());
     
-    function update_idr_value(){
-        console.log('Update IDR Value');
-        $('#total-asset').val($('#idr-balance').val());
-        
-        $.each(list_of_currency, function( index, value ) {
-            var request = $.ajax({
-                url: "https://vip.bitcoin.co.id/api/" + value + "_idr/ticker",
-                method: "GET",
-                dataType: "json"
-            });
-            
-            request.done(function( msg ) {
-                var idr_value = parseFloat($('#qty_' + value).val() * msg.ticker.last);
-                idr_value = parseFloat(idr_value - (3/1000 * idr_value)).toFixed(0);
-                $('#total-asset').val(parseFloat($('#total-asset').val()) + parseFloat(idr_value));
-                
-                $('#idr_value_' + value).html(numberWithCommas(msg.ticker.last) + ' IDR');
-                $('#times_idr_value_' + value).html(numberWithCommas(idr_value) + ' IDR');      
-                $('#estimates_asset').html(numberWithCommas($('#total-asset').val()) + ' IDR');    
-            });
-            
-            request.fail(function( jqXHR, textStatus ) {
-                alert( "Update " + value + " IDR value failed: " + textStatus );
-            });
+    $.each(list_of_currency, function( index, value ) {
+        var request = $.ajax({
+            url: "https://vip.bitcoin.co.id/api/" + value + "_idr/ticker",
+            method: "GET",
+            dataType: "json"
         });
-    }
-    
-    setInterval(update_idr_value(), 60000);
-    
+        
+        request.done(function( msg ) {
+            var idr_value = parseFloat($('#qty_' + value).val() * msg.ticker.last);
+            idr_value = parseFloat(idr_value - (3/1000 * idr_value)).toFixed(0);
+            $('#total-asset').val(parseFloat($('#total-asset').val()) + parseFloat(idr_value));
+            
+            $('#idr_value_' + value).html(numberWithCommas(msg.ticker.last) + ' IDR');
+            $('#times_idr_value_' + value).html(numberWithCommas(idr_value) + ' IDR');      
+            $('#estimates_asset').html(numberWithCommas($('#total-asset').val()) + ' IDR');    
+        });
+        
+        request.fail(function( jqXHR, textStatus ) {
+            alert( "Update " + value + " IDR value failed: " + textStatus );
+        });
+    });
+}
+
+function functionUpdateIDRValue() {
+    update_idr_func = setInterval(update_idr_value, 20000);
+}
+functionUpdateIDRValue();
+
+$(function () {
     $('[data-toggle="tooltip"]').tooltip()
     
     $('.refresh-frame').on('click', function(){
