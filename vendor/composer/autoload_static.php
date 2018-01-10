ece7eaa3ac7bd38a5bb32c504109f591d1f099a3
Ricky Nugroho <rocanowi@yahoo.co.id>
2018-01-10 16:55:49 +0700

    Add mobile detect library

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -20,11 +20,27 @@ class ComposerStaticInitd962fd3efe15d3172a3362acc0dff7d7
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'D' => 
+        array (
+            'Detection' => 
+            array (
+                0 => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/namespaced',
+            ),
+        ),
+    );
+
+    public static $classMap = array (
+        'Mobile_Detect' => __DIR__ . '/..' . '/mobiledetect/mobiledetectlib/Mobile_Detect.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInitd962fd3efe15d3172a3362acc0dff7d7::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInitd962fd3efe15d3172a3362acc0dff7d7::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInitd962fd3efe15d3172a3362acc0dff7d7::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInitd962fd3efe15d3172a3362acc0dff7d7::$classMap;
 
         }, null, ClassLoader::class);
     }
